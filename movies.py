1faa38038eba4c665490f3ea22d9130f30a2e373
tmcknight <tommymcknight@gmail.com>
2015-01-17 11:32:07 -0500

    Remove tiny poster icon in movie details list
    It was getting squished into a square and wouldn’t change if the movie
    was changed while Alfred was active.

--- movies.py
+++ movies.py
@@ -89,12 +89,12 @@ def show_movie_info(movie):
         return
 
     #get poster
-    urllib.urlretrieve("https://image.tmdb.org/t/p/w92" + movie['poster_path'], "poster.jpg")
+    #urllib.urlretrieve("https://image.tmdb.org/t/p/w92" + movie['poster_path'], "poster.jpg")
 
     wf.add_item(title = '%s (%s)' % (movie['title'], movie['release_date'][:4]),
                 subtitle = get_subtitle(omdb_info),
                 valid = True,
-                icon = "poster.jpg",
+                #icon = "poster.jpg",
                 arg = "file://" + wf.workflowdir + '/movie.html')
 
     #log.debug(wf.workflowdir)
